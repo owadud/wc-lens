3d252c07eacd0add32f0548ce97bb946efcf3e31
Owadud <owadud@Owaduds-MacBook-Air.local>
2025-07-10 21:54:23 +0600

    Javescript practice

---
+++ plugins/are-you/index.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+ * Plugin Name:       Are you
+ * Plugin URI:        https:owadud.fun/
+ * Description:       WooCommerce Reports.
+ * Version:           1.0.0
+ * Author:            Owadud
+ * Author URI:        https://owadud.fun/
+ 
+ */
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class AreYou{
+    function __construct(){
+        add_action('enqueue_block_editor_assets',array($this,'adminAssets'));
+    }
+
+    function adminAssets(){
+        wp_enqueue_script('ournewlocktype',plugin_dir_url(__FILE__) . 'test.js',array('wp-blocks','wp-element'));
+    }
+
+}
+$areyou = new AreYou();
